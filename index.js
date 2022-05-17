c2a0ad7bb6a136e6dc37c350309c36b10b1b2ca6
KitkouskiKalani-FS-1 <KMKitkouski@student.fullsail.edu>
2022-05-16 21:22:03 -0500

    Completed videos on assignment

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 const productRouter = require('./routes/Products')
+app.set('views', __dirname+'/templates')
+app.set('view engine', 'twig')
 
+
+app.get('/', (req,res) =>{
+    res.render("home", {name:"World!", users:[
+        {name: 'Kalani Kitkouski', email: 'kmkitkouski@gmail.com'},
+        {name: 'Brandon Kitkouski', email: 'bkitkouski@gmail.com'}
+    ]})
+})
 app.use("/products", productRouter)
+
 
 
 
