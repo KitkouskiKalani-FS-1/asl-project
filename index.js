ffee9c75144e55e0bc7cd26f9abcf77be0c1cdbc
KitkouskiKalani-FS-1 <KMKitkouski@student.fullsail.edu>
2022-05-30 23:00:11 -0500

    e-commerce assignment

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ app.use(fileUpload())
 const productRouter = require('./routes/Products')
 const variantsRouter = require('./routes/Variants')
 const imagesRouter = require('./routes/Images')
+const storeRouter = require('./routes/Store')
+const { index } = require('./controllers/Store')
 app.set('views', __dirname+'/templates')
 app.set('view engine', 'twig')
 
 
-app.get('/', (req,res) =>{
-    res.render("views/home")
-})
+// app.get('/', (req,res) =>{
+//     res.render("views/home")
+// })
+
+app.use("/store", storeRouter)
 app.use("/products", productRouter)
 app.use("/variants", variantsRouter)
 app.use("/images", imagesRouter)
+app.use("/", index)
 
 
 
